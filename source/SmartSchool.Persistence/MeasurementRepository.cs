c7f34e41aee758c51828fbe2f8f05f731a15186b
Rene Lochner <school@renelochner.at>
2020-03-25 17:38:39 +0100

    Implement "MeasurementRepository"

--- source/SmartSchool.Persistence/MeasurementRepository.cs
+++ source/SmartSchool.Persistence/MeasurementRepository.cs
@@ -1,4 +1,5 @@
-﻿using SmartSchool.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.Core.Contracts;
 using SmartSchool.Core.Entities;
 using System.Linq;
 
@@ -18,5 +19,36 @@ namespace SmartSchool.Persistence
             _dbContext.Measurements.AddRange(measurements);
         }
 
-   }
+        public Measurement[] GetMeasurementByLocationAndName(string location, string name)
+        {
+            return _dbContext
+                    .Measurements
+                    .Include(sensor => sensor.Sensor)
+                    .Where(sensor => sensor.Sensor.Location.Equals(location) && sensor.Sensor.Name.Equals(name))
+                    .OrderByDescending(measurement => measurement.Value)
+                    .ThenByDescending(measurement => measurement.Time)
+                    .Take(3)
+                    .ToArray();
+        }
+
+        public int GetMeasurementCountByLocationAndName(string location, string name)
+        {
+            return _dbContext
+                   .Measurements
+                   .Include(measurement => measurement.Sensor)
+                   .Where(measurement => measurement.Sensor.Location.Equals(location) && measurement.Sensor.Name.Equals(name))
+                   .Count();
+
+        }
+
+        public double GetCo2MeasurementsAvgByLocationAndRange(string location, int min, int max)
+        {
+            return _dbContext
+                    .Measurements
+                    .Include(sensor => sensor.Sensor)
+                    .Where(sensor => sensor.Sensor.Location.Equals(location) && sensor.Sensor.Name.Equals("co2"))
+                    .Where(sensor => sensor.Value > min && sensor.Value < max)
+                    .Average(sensor => sensor.Value);
+        }
+    }
 }
