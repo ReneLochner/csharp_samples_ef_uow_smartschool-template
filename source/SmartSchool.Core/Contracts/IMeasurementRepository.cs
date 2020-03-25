48138838f416066560b92e32ebf41814530d5497
Rene Lochner <school@renelochner.at>
2020-03-25 17:35:52 +0100

    Implement "IMeasurementRepository"

--- source/SmartSchool.Core/Contracts/IMeasurementRepository.cs
+++ source/SmartSchool.Core/Contracts/IMeasurementRepository.cs
@@ -5,5 +5,8 @@ namespace SmartSchool.Core.Contracts
     public interface IMeasurementRepository
     {
         void AddRange(Measurement[] measurements);
+        Measurement[] GetMeasurementByLocationAndName(string location, string name);
+        double GetCo2MeasurementsAvgByLocationAndRange(string location, int min, int max);
+        int GetMeasurementCountByLocationAndName(string location, string name);
     }
 }
